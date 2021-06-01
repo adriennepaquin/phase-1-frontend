7e10e806fbb5c46b41f09a7150112216f26000ca
nkusumo <nis.kusumo@gmail.com>
2021-06-01 13:22:43 -0700

    day 1 done

---
+++ index.js
@@ -0,0 +1,33 @@
+const recipeCards = document.querySelector('#card-container');
+
+fetch("http://localhost:3000/recipes")
+.then(res => res.json())
+.then(renderRecipes);
+
+function renderRecipes(recipesObj) {
+    console.log(recipesObj);
+    recipesObj.forEach(recipe => renderRecipe(recipe));
+}
+
+function renderRecipe(recipe) {
+    console.log(recipe["recipe-url"])
+    let recipeDiv = document.createElement('div');
+    let title = document.createElement('h3');
+    let image = document.createElement('img')
+    let ingredUl = document.createElement('ul');
+    let link = document.createElement('a');
+    let likeDiv = document.createElement('div');
+    let likeBtn = document.createElement('button');
+    let likes = document.createElement('p');
+
+    title.textContent = recipe.name;
+    image.src = recipe.image;
+    link.href = recipe["recipe-url"];
+    link.textContent = "Click here for recipe!"
+    likeBtn.textContent = " YUM ";
+    likes.textContent = `${recipe.hearts} likes`;
+
+    likeDiv.append(likes, likeBtn);
+    recipeDiv.append(title, image, link, likeDiv);
+    recipeCards.append(recipeDiv);
+}
